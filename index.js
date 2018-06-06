7fe1f0eef70bc8ad42edd424fa106d38ae66dc1b
grahamhenhen@gmail.com <grahamhenhen@gmail.com>
2018-06-06 13:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function shout(string) {
-  return string.toUpperCase()
+ return string.toUpperCase()
 }
 
 function whisper(string) {
   return string.toLowerCase() 
 }
+<<<<<<< HEAD
 
 function logShout(string) {
  return console.log(string.toUpperCase())
@@ -16,6 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
  if (string === string.toLowerCase()) return "I can't hear you!";
- else if (string === string.toUpperCase()) return "YES INDEED!"; 
- else if (string === "I love you, Grandma.") return "I love you, too.";   
+ if (string === string.toUpperCase()) return "YES INDEED!"; 
+ if (string === "I love you, Grandma.") return "I love you, too.";   
 }
+=======
+function logshout(string) {
+  console.log.toUpperCase
+}
+>>>>>>> 6ac3b10cd5a8bd0837e72b41ac3eae4dd66e5b6a
